4771280e3acfb4cf74d9161551828a8d5d1a1e57
Manabendra Saharia <manab@ucar.edu>
2017-12-29 17:45:18 -0700

    Kirk: Take us out

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='luit',
+      version='0.1',
+      description='For managing, analyzing, and visualizing SUMMA Hydrologic Model inputs and outputs',
+      url='http://github.com/msaharia/luit',
+      author='Manabendra Saharia (Manab)',
+      author_email='manab@ucar.edu',
+      license='MIT',
+      packages=['xarray'],
+      zip_safe=False)
