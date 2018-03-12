8fe2f3ef03ba662a8c581bb80715460d67a683a2
msaharia <manab@ucar.edu>
2018-03-12 15:45:14 -0600

    Update

--- luit/temporal.py
+++ luit/temporal.py
@@ -1,2 +1,12 @@
-def test():
-    print('s')
+import matplotlib.pyplot as plt
+
+def plotncvar(ncdat, ncvar, starttime, endtime, xlabel, ylabel):
+    '''
+    Plots netCDF variables temporally. Start and End time 
+    set globally
+    '''
+    plt.figure(figsize=(15, 5))
+    ncdat[ncvar].loc[starttime:endtime].plot()
+    plt.ylabel(xlabel, fontsize = 16)
+    plt.xlabel(ylabel, fontsize = 16)
+    plt.show()
